21c5ac15b17260881dcb956d17f8214a3837841c
zhf <13159433@qq.com>
2024-07-12 15:43:13 +0800

    Initial commit

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,43 @@
 <?php
+
+if (!function_exists('scan_directory')) {
+    function scan_directory(
+        $directory,
+        &$controllersMethods,
+        $excludeMethods,
+        $baseNamespace = 'App\\Http\\Controllers'
+    ): void {
+        $items = array_filter(scandir($directory), function ($entry) {
+            return $entry !== '.' && $entry !== '..';
+        });
+
+        foreach ($items as $item) {
+            $path = $directory . '/' . $item;
+            if (is_dir($path)) {
+                scan_directory($path, $controllersMethods, $excludeMethods, $baseNamespace);
+            } elseif (is_file($path) && pathinfo($path, PATHINFO_EXTENSION) === 'php') {
+                $relativeNamespace = str_replace(realpath(app_path('Http/Controllers')), '', $directory);
+                $relativeNamespace = trim(str_replace(DIRECTORY_SEPARATOR, '\\', $relativeNamespace), '\\');
+                $namespace = $baseNamespace . '\\' . $relativeNamespace;
+                $className = pathinfo($path, PATHINFO_FILENAME);
+                $fullClassName = $namespace . '\\' . $className;
+
+                if (class_exists($fullClassName)) {
+                    $reflectionClass = new ReflectionClass($fullClassName);
+                    $methods = $reflectionClass->getMethods(ReflectionMethod::IS_PUBLIC);
+                    $methodNames = [];
+
+                    foreach ($methods as $method) {
+                        if ($method->class == $fullClassName && !in_array($method->name, $excludeMethods)) {
+                            $methodNames[] = $method->name;
+                        }
+                    }
+
+                    $prefix = str_replace('\\', '/', $relativeNamespace);
+                    $prefix = Str::lower(implode('/', array_map('ucfirst', explode('/', $prefix))));
+                    $controllersMethods[$prefix] = ['className' => $fullClassName, 'methods' => $methodNames];
+                }
+            }
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,69 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-$directory = app_path('Http/Controllers');
-$subfolders = array_filter(scandir($directory), function ($entry) use ($directory) {
-    return $entry !== '.' && $entry !== '..' && is_dir($directory . '/' . $entry);
-});
-
 $controllersMethods = [];
-$excludeMethods = [
-    '__construct',
-    'middleware'
-];
+$directory = app_path('Http/Controllers');
+$excludeMethods = ['__construct', 'middleware'];
 
-foreach ($subfolders as $subfolder) {
-    $subfolderPath = $directory . '/' . $subfolder;
-    $files = array_filter(scandir($subfolderPath), function ($file) use ($subfolderPath) {
-        return is_file($subfolderPath . '/' . $file) && pathinfo($file, PATHINFO_EXTENSION) === 'php';
-    });
+scan_directory($directory, $controllersMethods, $excludeMethods);
 
-    foreach ($files as $file) {
-        $className = pathinfo($file, PATHINFO_FILENAME);
-        $fullClassName = "App\\Http\\Controllers\\$subfolder\\$className";
-
-        if (class_exists($fullClassName)) {
-            $reflectionClass = new ReflectionClass($fullClassName);
-            $methods = $reflectionClass->getMethods(ReflectionMethod::IS_PUBLIC);
-            $methodNames = [];
-
-            foreach ($methods as $method) {
-                if ($method->class == $fullClassName && !in_array($method->name, $excludeMethods)) {
-                    $methodNames[] = $method->name;
-                }
-            }
-
-            $controllersMethods[$fullClassName] = $methodNames;
-        }
-    }
-}
-
+/**
+ * Do not modify this file, routes will be generated automatically
+ */
 Route::prefix('api')->middleware(['throttle:60,1'])->group(function () use ($controllersMethods) {
-    foreach ($controllersMethods as $fullClassName => $methods) {
-        $prefix = Str::lower(explode('\\', $fullClassName)[3]);
-        Route::prefix($prefix)->group(function () use ($fullClassName, $methods, $prefix) {
-            foreach ($methods as $method) {
+    foreach ($controllersMethods as $prefix => $controllerInfo) {
+        Route::prefix($prefix)->group(function () use ($controllerInfo, $prefix) {
+            foreach ($controllerInfo['methods'] as $method) {
                 Route::post(
                     Str::replace('_', '-', Str::snake($method)),
-                    "$fullClassName@$method"
-                )
-                    ->name($prefix . '.' . $method);
+                    "{$controllerInfo['className']}@$method"
+                )->name(Str::replace('/', '.', $prefix) . '.' . $method);
             }
         });
     }
 });
-
-
-//Route::prefix('users')->group(function () {
-//    Route::post('login', [UserController::class, 'login']);
-//    Route::post('register', [UserController::class, 'register']);
-//
-//    Route::middleware(['auth:sanctum'])->group(function () {
-//        Route::post('logout', [UserController::class, 'logout']);
-//        Route::post('info', [UserController::class, 'info'])->middleware('acl:user.info');
-//        Route::post('permissions', [UserController::class, 'permissions']);
-//        Route::post('roles', [UserController::class, 'roles']);
-//        Route::post('update-password', [UserController::class, 'updatePassword']);
-//        Route::post('update-profile', [UserController::class, 'updateProfile']);
-//    });
-//});
